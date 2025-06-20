1e3f2a2059b3d3bfb43a651d01aab92493421e3c
Camper-CoolDie <campercooldie@gmail.com>
2025-06-20 21:32:53 +0300

    add support down to 1.21.2

--- Common/src/main/java/com/cooldie/jesustotem/renderer/JesusRenderer.java
+++ Common/src/main/java/com/cooldie/jesustotem/renderer/JesusRenderer.java
@@ -38,7 +38,7 @@ public class JesusRenderer {
         int guiWidth = graphics.guiWidth();
         int guiHeight = graphics.guiHeight();
         graphics.blit(
-            RenderType::blockScreenEffect,
+            RenderType::guiTexturedOverlay,
             JESUS_TEXTURES[jesusIndex],
             0, 0,
             0f, 0f,
